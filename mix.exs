6c9cbce4bf4125ae4a369a003ed6dcb25720caa3
Isaak Tsalicoglou <isaak@waseigo.com>
2023-03-06 03:15:24 +0200

    improved description

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule VatchexGreece.MixProject do
   def project do
     [
       app: :vatchex_greece,
-      version: "0.5.1",
+      version: "0.5.2",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       description: description(),
@@ -16,7 +16,8 @@ defmodule VatchexGreece.MixProject do
       source_url: "https://github.com/waseigo/vatchex_greece",
       homepage_url: "https://github.com/waseigo/vatchex_greece",
       docs: [
-        main: "VatchexGreece", # The main page in the docs
+        # The main page in the docs
+        main: "VatchexGreece",
         logo: "./logo.png",
         extras: ["README.md"]
       ]
@@ -25,8 +26,7 @@ defmodule VatchexGreece.MixProject do
 
   defp description do
     """
-    Library for fetching information on Greek companies based on their
-    VAT number from the GSIS SOAP web service.
+    An Elixir library to easily pull company information from the SOAP web service of the Greek General Secretariat of Information Systems for Public Administration (GSIS) using the VAT ID (Αριθμός Φορολογικού Μητρώου, abbreviated as "ΑΦΜ" or "Α.Φ.Μ.").
     """
   end
 
@@ -50,9 +50,10 @@ defmodule VatchexGreece.MixProject do
   defp deps do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
+      # added for parsing SOAP response
       {:soap, "~> 1.0"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.27", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.27", only: :dev, runtime: false}
     ]
   end
 end
