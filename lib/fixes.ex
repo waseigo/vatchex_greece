bfa908f65a429b86da35d20658ca93f78056dd0f
Isaak Tsalicoglou <isaak@waseigo.com>
2023-03-08 00:42:23 +0200

    fixes now work with a response tuple so that errors bubble up

--- lib/fixes.ex
+++ lib/fixes.ex
@@ -3,7 +3,7 @@ defmodule VatchexGreece.Fixes do
   # SPDX-License-Identifier: Apache-2.0
 
   @moduledoc """
-  Fix/correct some elements of the response.
+  Fix/correct some elements of the response map.
   """
   @moduledoc since: "0.4.0"
 
@@ -14,49 +14,56 @@ defmodule VatchexGreece.Fixes do
   Apply all fixes, i.e. fix activities list, unquote commercial if quoted, and
   convert the registration date to YYYY-MM-DD format.
   """
-  def apply_all(m) do
-    m
-    |> fix_activities_list()
-    |> unquote_commercial_title()
-    |> make_iso_registration_date()
+  def apply_all(response_tuple) do
+    case response_tuple do
+      {:ok, response_map} ->
+        {:ok,
+         response_map
+         |> fix_activities_list()
+         |> unquote_commercial_title()
+         |> make_iso_registration_date()}
+
+      _ ->
+        response_tuple
+    end
   end
 
-  defp fix_activities_list(m) do
+  defp fix_activities_list(response_map) do
     key = @key_activities
 
     activities =
-      m[key]
+      response_map[key]
       |> Enum.map(fn {_, v} -> {"n" <> v.firmActCode, v} end)
       |> Map.new()
 
-    %{m | key => activities}
+    %{response_map | key => activities}
   end
 
-  defp unquote_commercial_title(m) do
+  defp unquote_commercial_title(response_map) do
     keys = [@key_information, :commerTitle]
-    current_value = get_in(m, keys)
+    current_value = get_in(response_map, keys)
     to_replace = "\""
 
     if current_value != %{} and String.contains?(current_value, to_replace) do
       new_value =
-        get_in(m, keys)
+        get_in(response_map, keys)
         |> String.replace(to_replace, "")
 
-      put_in(m, keys, new_value)
+      put_in(response_map, keys, new_value)
     else
-      m
+      response_map
     end
   end
 
-  defp make_iso_registration_date(m) do
+  defp make_iso_registration_date(response_map) do
     keys = [@key_information, :registDate]
 
     new_value =
-      get_in(m, keys)
+      get_in(response_map, keys)
       |> String.slice(0, 10)
       |> Date.from_iso8601!()
       |> Date.to_string()
 
-    put_in(m, keys, new_value)
+    put_in(response_map, keys, new_value)
   end
 end
