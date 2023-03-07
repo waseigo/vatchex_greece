1c0082b6f0486cb58edb377cb40f437e3fbdf92f
Isaak Tsalicoglou <isaak@waseigo.com>
2023-03-08 00:40:35 +0200

    fixed remnants of old name

--- test/grvatchex_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule GrvatchexTest do
-  use ExUnit.Case
-  doctest Grvatchex
-
-  test "greets the world" do
-    assert Grvatchex.hello() == :world
-  end
-end
---
+++ test/vatchex_greece_test.exs
@@ -0,0 +1,8 @@
+defmodule VatchexGreeceTest do
+  use ExUnit.Case
+  doctest VatchexGreece
+
+  # test "greets the world" do
+  #   assert VatchexGreece.hello() == :world
+  # end
+end
