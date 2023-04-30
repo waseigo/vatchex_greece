027a0975fad9f618910b2336dc298c69e76830d3
Isaak Tsalicoglou <isaak@waseigo.com>
2023-04-30 20:13:26 +0300

    improved error handling conforming to the new way of using structs to handle the data

--- lib/request.ex
+++ lib/request.ex
@@ -15,22 +15,86 @@ defmodule VatchexGreece.Request do
   Prepare the request's XML template based on the VAT ID you call for),
   and authentication settings (username, password, VAT ID you call from).
   """
+  @doc since: "0.5.0"
   # statically compile the XML request template and create a function to customize it
   EEx.function_from_file(
     :def,
-    :prepare,
+    :to_xml,
     "lib/request.xml.eex",
     [:afm_called_for, :username, :password, :afm_called_by]
   )
 
+  def prepare({:ok, %Results{auth: auth, data: data} = input}) do
+    xml =
+      to_xml(
+        data.afm,
+        auth.username,
+        auth.password,
+        auth.afm_called_by
+      )
+
+    {:ok, %Results{input | request: xml}}
+  end
+
+  def prepare({:error, input}) do
+    {:error, input}
+  end
+
   @doc """
   POST the XML request with proper headers to the SOAP web service.
   """
-  def post(xml) do
-    HTTPoison.post(
-      @gsis_wsdl_url,
-      xml,
-      %{"Content-Type" => "application/soap+xml"}
-    )
+  @doc since: "0.5.0"
+  def post({:ok, %Results{request: xml, errors: errors} = input}) do
+    response =
+      HTTPoison.post(
+        @gsis_wsdl_url,
+        xml,
+        %{"Content-Type" => "application/soap+xml"}
+      )
+
+    case response do
+      {:error, http_response} ->
+        errors =
+          Map.put(
+            errors,
+            :http_request_error,
+            "HTTP request error."
+          )
+
+        {:error, %Results{input | response: http_response, errors: errors}}
+
+      {:ok, http_response} ->
+        case http_response.status_code do
+          200 ->
+            handle_status_200(input, http_response)
+
+          _ ->
+            errors =
+              Map.put(
+                errors,
+                :http_not_OK,
+                "HTTP status code #{http_response.status_code} (not OK)."
+              )
+
+            {:error, %Results{input | response: http_response, errors: errors}}
+        end
+    end
+  end
+
+  def post({:error, input}) do
+    {:error, input}
+  end
+
+  # refactored function to void 3-levels-deep case handling in post/1
+  defp handle_status_200(%Results{errors: errors} = input, http_response) do
+    if String.contains?(
+         http_response.body,
+         "RG_WS_PUBLIC_TOKEN_USERNAME_NOT_AUTHENTICATED"
+       ) do
+      errors = Map.put(errors, :authentication_error, "Authentication error.")
+      {:error, %Results{input | response: http_response, errors: errors}}
+    else
+      {:ok, %Results{input | response: http_response}}
+    end
   end
 end
