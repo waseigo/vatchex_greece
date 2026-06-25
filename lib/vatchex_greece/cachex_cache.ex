aa9bcc21bea19b11ddf7830ba42994d474e129e1
Isaak Tsalicoglou <isaak@waseigo.com>
2026-06-26 01:06:29 +0300

    add Cachex v4 adapter module

---
+++ lib/vatchex_greece/cachex_cache.ex
@@ -0,0 +1,41 @@
+# SPDX-FileCopyrightText: 2023 Isaak Tsalicoglou <isaak@waseigo.com>
+# SPDX-License-Identifier: Apache-2.0
+
+if Code.ensure_loaded?(Cachex) do
+  defmodule VatchexGreece.CachexCache do
+    @moduledoc """
+    Cachex adapter for `VatchexGreece.Cache` protocol.
+
+    Uses a Cachex instance (defaults to a cache named `:vatchex_greece`).
+    Configure the cache name in your application config:
+
+        config :vatchex_greece, :cache_name, :my_custom_cache
+
+    The Cachex instance must be started in your supervision tree:
+
+        children = [
+          {Cachex, name: :vatchex_greece, limit: 10_000},
+          ...
+        ]
+    """
+
+    def get(cache, key) do
+      case Cachex.get(cache, key) do
+        {:ok, nil} -> :miss
+        {:ok, value} -> {:ok, value}
+        _ -> :miss
+      end
+    end
+
+    def put(cache, key, value, ttl \\ cache_ttl()) do
+      Cachex.put(cache, key, value, expiration: ttl)
+    end
+
+    defp cache_ttl, do: Application.get_env(:vatchex_greece, :cache_ttl, 3_600_000)
+  end
+
+  defimpl VatchexGreece.Cache, for: VatchexGreece.CachexCache do
+    def get(cache, key), do: VatchexGreece.CachexCache.get(cache, key)
+    def put(cache, key, value, ttl), do: VatchexGreece.CachexCache.put(cache, key, value, ttl)
+  end
+end
